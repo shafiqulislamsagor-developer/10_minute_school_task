8e55c2637be74d3ca418e7838fa9d469521058f3
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-07-26 07:42:33 +0600

    NEW CHANGES: 2025-07-26 07:42:33 AM

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
     domains: ["cdn.10minuteschool.com", "s3.ap-southeast-1.amazonaws.com"],
   },
